3b13bb42478770233e29184d5af1c48a3587e60b
David <gelpiorama@gmail.com>
2018-11-05 17:19:53 +0100

    inheritance object chain

--- inheritance_object_chain.js
+++ inheritance_object_chain.js
@@ -1,3 +1,20 @@
+
+// inherit() returns a newly created object that inherits properties from the
+// prototype object p.  It uses the ECMAScript 5 function Object.create() if
+// it is defined, and otherwise falls back to an older technique.
+
+function inherit(p) {
+    if (p == null) throw TypeError();   // p must be a non-null object
+    if (Object.create)                  // If Object.create() is defined...
+        return Object.create(p);        //    then just use it.
+    var t = typeof p;                   // Otherwise do some more type checking
+    if (t !== "object" && t !== "function") throw TypeError();
+    function f() {};                    // Define a dummy constructor function.
+    f.prototype = p;                    // Set its prototype property to p.
+    return new f();                     // Use f() to create an "heir" of p.
+}
+
+
 /**
  * Como funciona la resolucion de nombre en la cadena de objetos
  * del siguiente codigo?
@@ -30,4 +47,4 @@ var c = inherit(unitcircle);    // c inherits the property r
 
 c.x = 1; c.y = 1;               // c defines two properties of its own
 c.r = 2;                        // c overrides its inherited property
-cpnsole.log(unitcircle.r);      // => 1: the prototype object is not affected
+console.log(unitcircle.r);      // => 1: the prototype object is not affected
--- testing_properties.js
+++ testing_properties.js
@@ -12,3 +12,14 @@ var o = { x: 1 };
 console.log("x" in o);                // true: o has an own property "x"
 console.log("y" in o);                // false: o doesn't have a property "y"
 console.log("toString" in o);         // true: o inherits a toString property
+
+/**
+ * The hasOwnProperty() method of an object tests whether
+ * that object has an own property with the given name. 
+ * It returns false for inherited properties.
+ */
+
+var o = { x: 1 };
+console.log(o.hasOwnProperty("x"));                  // true: o has an own property x
+console.log(o.hasOwnProperty("y"));                  // false: o doesn't have a property y
+console.log(o.hasOwnProperty("toString"));           // false: toString is an inherited property
