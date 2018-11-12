289590f20adfa10b22597004d0ec1d7a52703aac
David <gelpiorama@gmail.com>
2018-11-12 11:55:49 +0100

    testing properties con mensajes a consola

--- testing_properties.js
+++ testing_properties.js
@@ -9,9 +9,9 @@
 
 
 var o = { x: 1 };
-console.log("x" in o);                // true: o has an own property "x"
-console.log("y" in o);                // false: o doesn't have a property "y"
-console.log("toString" in o);         // true: o inherits a toString property
+console.log("x es propiedad de o:", "x" in o);                // true: o has an own property "x"
+console.log("y no es propiedad de o:", "y" in o);                // false: o doesn't have a property "y"
+console.log("toString se hereda y esta en o:", "toString" in o);         // true: o inherits a toString property
 
 /**
  * The hasOwnProperty() method of an object tests whether
@@ -20,6 +20,6 @@ console.log("toString" in o);         // true: o inherits a toString property
  */
 
 var o = { x: 1 };
-console.log(o.hasOwnProperty("x"));                  // true: o has an own property x
-console.log(o.hasOwnProperty("y"));                  // false: o doesn't have a property y
-console.log(o.hasOwnProperty("toString"));           // false: toString is an inherited property
+console.log("x es propiedad de o: " + o.hasOwnProperty("x"));                  // true: o has an own property x
+console.log("y no existe en o: " + o.hasOwnProperty("y"));                  // false: o doesn't have a property y
+console.log("toString se hereda, no es propiedad de o: " + o.hasOwnProperty("toString"));           // false: toString is an inherited property
